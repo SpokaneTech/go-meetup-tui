f34f273e83600224421634fafa157d57e3a6f64b
Seth Back <sethjback@gmail.com>
2025-03-12 14:42:51 -0700

    updates

--- cmd/sql/main.go
+++ cmd/sql/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
+	"strconv"
 	"strings"
 
 	"github.com/SpokaneTech/go-meetup-tui/pkg/db"
+	"github.com/charmbracelet/bubbles/table"
 	"github.com/charmbracelet/bubbles/textarea"
 	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/charmbracelet/bubbles/viewport"
@@ -42,7 +43,22 @@ func initModel() Model {
 	ti.CharLimit = 156
 	ti.Width = 20
 
+	rt := table.New()
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false).
+		Align(lipgloss.Left)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	rt.SetStyles(s)
+
 	return Model{
+		results:  rt,
 		open:     ti,
 		viewport: vp,
 		query:    ta,
@@ -53,10 +69,12 @@ func initModel() Model {
 // It must satisfy the tea `model` interface
 type Model struct {
 	viewport viewport.Model
+	results  table.Model
 	query    textarea.Model
 	open     textinput.Model
 
 	err string
+	msg string
 
 	db db.DB
 }
@@ -73,9 +91,10 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
+		m.err = ""
 		key := msg.String()
 		switch key {
-		case "ctrl+c", "q":
+		case "ctrl+c":
 			// return the model, and ask for tea to quit
 			return m, tea.Quit
 
@@ -83,16 +102,21 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			m.query.SetValue("")
 
 		case "enter":
-			m.err = ""
+			m.msg = ""
 			if m.db == nil {
 				m.open.Blur()
 				dbCmd = openDB(m.open.Value())
 			} else {
-				m.viewport.SetContent("")
 				m.query.Blur()
 				dbCmd = queryDB(m.db, m.query.Value())
 			}
 		}
+
+	case tea.WindowSizeMsg:
+		m.viewport.Width = msg.Width
+		m.query.SetWidth(msg.Width)
+		m.viewport.Height = (msg.Height / 2) - m.query.Height()
+		m.results.SetHeight((msg.Height / 2) - m.query.Height())
 
 	case DBOpen:
 		m.db = msg.db
@@ -101,8 +125,21 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	case DBQuery:
 		m.query.Focus()
-		d, _ := json.Marshal(msg.rows)
-		m.viewport.SetContent(string(d))
+		if len(msg.rows) == 0 {
+			m.msg = "success"
+		} else {
+			cols := []table.Column{}
+			for _, c := range msg.colnames {
+				cols = append(cols, table.Column{Title: c, Width: lipgloss.Width(c) + 6})
+			}
+
+			rows := []table.Row{}
+			for _, r := range msg.rows {
+				rows = append(rows, convertRow(r, msg.colnames))
+			}
+			m.results.SetColumns(cols)
+			m.results.SetRows(rows)
+		}
 
 	case errMsg:
 		if m.db == nil {
@@ -124,11 +161,18 @@ func (m Model) View() string {
 	doc := strings.Builder{}
 
 	if m.db == nil {
-		doc.WriteString(fmt.Sprintf("Please enter a DB filename to open:\n\n%s\n\n(q to quit)", m.open.View()))
+		doc.WriteString(fmt.Sprintf("Please enter a DB filename to open:\n\n%s\n\n(ctrl+c to quit)", m.open.View()))
 	} else {
-		doc.WriteString(fmt.Sprintf("\n%s\n\n%s\n\n", m.query.View(), m.viewport.View()))
+		doc.WriteString(fmt.Sprintf("\n%s\n\n", m.query.View()))
+
 		if m.err != "" {
 			doc.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("9")).Render(fmt.Sprintf("error: %s\n", m.err)))
+		}
+
+		if m.msg != "" {
+			doc.WriteString(lipgloss.NewStyle().Foreground(lipgloss.Color("#04B575")).Render(fmt.Sprintf("%s\n", m.msg)))
+		} else {
+			doc.WriteString(fmt.Sprintf("\n\n%s\n\n", m.results.View()))
 		}
 	}
 
@@ -180,4 +224,29 @@ func queryDB(db db.DB, query string) func() tea.Msg {
 
 		return DBQuery{colnames: c, rows: r}
 	}
+}
+
+func convertRow(r interface{}, cnames []string) table.Row {
+	mr, ok := r.(map[string]interface{})
+	if !ok {
+		return nil
+	}
+
+	row := []string{}
+	for _, n := range cnames {
+		switch v := mr[n].(type) {
+		case bool:
+			row = append(row, strconv.FormatBool(v))
+		case string:
+			row = append(row, v)
+		case int64:
+			row = append(row, strconv.FormatInt(v, 0))
+		case int32:
+			row = append(row, string(v))
+		case float64:
+			row = append(row, strconv.FormatFloat(v, 'f', -1, 64))
+		}
+	}
+
+	return row
 }
