8fbe49d7db89ffb3a28b0b3c0379ab82af1c2be1
Seth Back <sethjback@gmail.com>
2025-03-12 13:30:32 -0700

    update to workshop

--- cmd/gloss/main.go
+++ cmd/gloss/main.go
@@ -19,13 +19,7 @@ func main() {
 
 // Model is the main container for our tui.
 // It must satisfy the tea `model` interface
-type Model struct {
-	bold       bool
-	strike     bool
-	italic     bool
-	underline  bool
-	background bool
-}
+type Model struct{}
 
 // Init the tui. The returned tea.Cmd represents any initial
 // I/O or setup type functions we want to execute
@@ -42,16 +36,6 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			// return the model, and ask for tea to quit
 			return m, tea.Quit
-		case "b":
-			m.bold = !m.bold
-		case "B":
-			m.background = !m.background
-		case "s":
-			m.strike = !m.strike
-		case "i":
-			m.italic = !m.italic
-		case "u":
-			m.underline = !m.underline
 		}
 	}
 
@@ -63,6 +47,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 var (
 	highlightColor = lipgloss.AdaptiveColor{Light: "#5bfa11", Dark: "#5fdb25"}
 	textColor      = lipgloss.AdaptiveColor{Light: "#0d0000", Dark: "#fcfcfc"}
+	bgColor        = lipgloss.Color("#87b074")
+	fgColor        = lipgloss.Color("#0d0000")
 	windowStyle    = lipgloss.NewStyle().
 			BorderForeground(highlightColor).
 			Foreground(textColor).
@@ -77,13 +63,8 @@ var (
 func (m Model) View() string {
 	doc := strings.Builder{}
 
-	renderStyle := windowStyle.Bold(m.bold).Italic(m.italic).Strikethrough(m.strike).Underline(m.underline)
-	if m.background {
-		renderStyle = renderStyle.Background(lipgloss.Color("#87b074")).Foreground(lipgloss.Color("#0d0000"))
-	}
-
-	doc.WriteString(renderStyle.Render("has style much!"))
-	doc.WriteString(helpStyle.Render("\nq: exit • b: bold • s: strike • i: italic • u: underline • B: background"))
+	// doc.WriteString(renderStyle.Render("has style much!"))
+	// doc.WriteString(helpStyle.Render("\nq: exit • b: bold • s: strike • i: italic • u: underline • B: background"))
 
 	return doc.String()
 }
